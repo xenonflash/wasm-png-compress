ff711d866d6c07be62aee7d71e06bca9708c87a6
yankechao <yankechao@vipkid.com.cn>
2020-04-15 11:40:28 +0800

    更新脚本

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/Antelle/Projects/wasm-image-compressor
+# Install script for directory: /Users/yankechao/Documents/vipkid/wasm-image-compressor
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,15 +27,10 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Is this installation the result of a crosscompile?
-if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "TRUE")
-endif()
-
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/Antelle/Projects/wasm-image-compressor/lib/zlib/cmake_install.cmake")
-  include("/Users/Antelle/Projects/wasm-image-compressor/lib/libpng/cmake_install.cmake")
+  include("/Users/yankechao/Documents/vipkid/wasm-image-compressor/lib/zlib/cmake_install.cmake")
+  include("/Users/yankechao/Documents/vipkid/wasm-image-compressor/lib/libpng/cmake_install.cmake")
 
 endif()
 
@@ -47,5 +42,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/Antelle/Projects/wasm-image-compressor/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/yankechao/Documents/vipkid/wasm-image-compressor/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
